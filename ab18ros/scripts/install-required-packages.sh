872a22502d44f7c7ce76bb4ce5f3a205ff6a40e1
Emir Cem Gezer <emircem.gezer@gmail.com>
2018-03-23 00:24:55 +0300

    install package without asking

--- ab18ros/scripts/install-required-packages.sh
+++ ab18ros/scripts/install-required-packages.sh
@@ -9,6 +9,6 @@ DIR=$(rospack find ab18ros)
 PACKAGES=$(find $DIR/.. -name package.xml | rev | cut -d/ -f2 | rev)
 for package in $PACKAGES; do 
   echo "$(tput bold)Scanning dependencies for $package$(tput sgr0)"
-  rosdep install $package --rosdistro $ROSDISTRO --os $DISTRO:$CODENAME 
+  rosdep install -y $package --rosdistro $ROSDISTRO --os $DISTRO:$CODENAME 
   echo "===="
 done 
